32addbc9c7d96924cba3ae9ce9dcf8136df013df
Biel Sanz <biel.sanz@estudiants.urv.cat>
2025-10-08 15:27:08 +0200

    "checkout init"

--- PracticaCompus/source/candy1_init.s
+++ PracticaCompus/source/candy1_init.s
@@ -52,73 +52,67 @@ inicializa_matriz:
 		mov r0, #COLUMNS
 		mov r2, #ROWS
 		mul r0, r2, r0
+
 		mul r5, r0, r1 @; offset del mapa que juguem
 
 		ldr r4,=mapas @; direccio on comença la configuracio de mapes
 
 		mov r8, #0 @; offset per la direccio base de la matriu
 
-		mov r2, #0			@;R5 = contador de columna (inicialment 0)
+		mov r1, #0			@;R5 = contador de columna (inicialment 0)
 		.lseguentfila:
-		mov r1, #0			@;R4 = contador de filas(inicialment 0)		
+		mov r2, #0			@;R4 = contador de filas(inicialment 0)		
 
 		.lseguentcolumna:
 
-		ldr r6, [r4, r5] @;primer valor del mapa que volem jugar
-		add r5, r5, #1 @; movem un byte
+		ldrb r6, [r4, r5] @;primer valor del mapa que volem jugar
+		strb r6, [r7, r8]
+		mov r9, r6 @; per no perdre el valor
+
+		and r6, #MASK_GEL
+		cmp r6, #0
+		bne .Lseguent_iteracio_init
+
 
 
 		.Lgenerar_caramel:   @; mejora necesaria: cambiarlo por mascaras
+
+		
+		mov r6, #3
+		lsl r6, #3
+		and r6, r9 @; guardem a r6 les marques de gelatines
+
+
 		mov r0, #6 @; per a obtenir numero entre 0-5 
-		cmp r6, #0		@;comprobar si el tile es 0, 8 o 16 para generar
-		bleq mod_random
-		cmp r6, #8
-		bleq mod_random
-		cmp r6, #16
-		bleq mod_random
-
-		addeq r0, r0, #1
-
+		bl mod_random
+		add r0, r0, #1
+		add r0, r6 @; li afegim la gelatina
 
 		mov r10 , #0 @; contador de horizontales (este-oeste)
 		mov r11, #0 @; contador verticales (norte-sur)
 
 
-		addeq r0, r6, r0
-		strb r0, [r7,r8]
 
 
-		.Lcomprobar_repetits:
-		mov r3, #0
-		mov r0, r7
 
-		bl cuenta_repeticiones
-
-
-		cmp r3, #0
-		add r3, #2
-		addeq r10, r10, r0
-		b .Lcomprobar_repetits
 
 		
-		cmp r3, #1
-		add r3, #1
-		addeq r11, r11, r0
-		b .Lcomprobar_repetits
+		strb r0, [r7,r8]
+		mov r3, #2
+		
+		.Lcomprobar_repetits:
+
+		mov r0, r7 @; movem a r0 la direccio de memoria
+		bl cuenta_repeticiones
 		
 		cmp r3, #2
-		add r3, #2
+		addeq r3, #1
 		addeq r10, r10, r0
-		b .Lcomprobar_repetits
+		beq .Lcomprobar_repetits
 		
 		cmp r3, #3		
-		add r3, #1
 		addeq r11, r11, r0
-		b .Lcomprobar_repetits
-
-		sub r10, r10, #2
-		sub r11, r11, #2
-
+		
 		cmp r10, #3
 		bhs .Lgenerar_caramel
 		cmp r11, #3
@@ -127,19 +121,19 @@ inicializa_matriz:
 
 
 
+		.Lseguent_iteracio_init:
+	
 		add r8, #1 @; movem un byte
+		add r5, #1 @; movem un byte
 
-		
-		add r1, #1
-		cmp r1, #COLUMNS		@;comprobar si se han recorrido todas las columnas
-		bne .la_seguentcolumna
-		
-		add r2, #1 
-		cmp r2, #ROWS	@;comprobar si se han recorrido todas las filas
-		bne .la_seguentfila
+		add r2, r2,#1
+		cmp r2, #COLUMNS		@;comprobar si se han recorrido todas las columnas
+		bne .lseguentcolumna
+		add r1, r1 ,#1 
+		cmp r1, #ROWS	@;comprobar si se han recorrido todas las filas
+		bne .lseguentfila
 
 		pop {r0-r11,pc}			@;recuperar registros y retornar al invocador
-
 
 @;TAREA 1B;
 @; recombina_elementos(*matriz): rutina para generar una nueva matriz de juego
@@ -161,7 +155,7 @@ inicializa_matriz:
 @;		R0 = dirección base de la matriz de juego
 	.global recombina_elementos
 recombina_elementos:
-		push {r0-r7, lr}
+		push {r0-r11, lr}
 		
 		mov r7, r0 				@; conservem direcció base de memòria a r7
 
@@ -185,31 +179,37 @@ recombina_elementos:
 			cmp r3, #ROWS*COLUMNS
 			blo .Lcopia_matriu
 
-
+		mov r10, #0
 		mov r1, #0 @; iterador fila
 		.lb_seguentfila:
 		mov r2, #0 @; iterador de columna
 
 		
 		.lb_seguentcolumna:
-
+		
+		mov r11, #10
 		.Lrecombinacio:
 			ldrb r4, [r7, r10]
 			and r5, r4, #0x7
-			cmp r5, #0			@; si l'element de la matriu base és buida, forat o sòlid ignorem
+			cmp r5, #0			 @; el control del 0 no té sentit a nivell algorismic doncs mai recombinariem 
+			                     @; un valor de caramel no inicialitzat pero igualment comptarem aquest cas
 			beq .Lseguent_iteracio_base
-			cmp r5, #0x7
+			cmp r5, #0x7        @; si l'element de la matriu base és buida, forat o sòlid ignorem
 			beq .Lseguent_iteracio_base
 
 			mov r6, r4, lsr #3	
-			and r6, #0x03		@; en teoria reduntant r6 = codi de gelatina
+			@;and r6, #0x03		@; en teoria reduntant r6 = codi de gelatina
 
 			.Lcasella_aleatoria:
 				mov r0, #ROWS*COLUMNS
 				bl mod_random
 				ldrb r4, [r8, r0] 	@; obtenir posició aleatòria de mat_recomb1[][]
-				cmp r4, #0
+				cmp r4, #0			
+
 				beq .Lcasella_aleatoria @; torna a intentar si la posició conté un 0
+
+			mov r5, #0
+			strb r5, [r8, r0]	@; fixem 0 a la posició visitada de mat_recomb1[][]
 
 			@; afegim el codi de la gelatina de la casella
 			cmp r6, #2
@@ -217,16 +217,28 @@ recombina_elementos:
 			cmpne r6, #1
 			addeq r4, #8
 
-			strb r4, [r9, r10]
+
+
+
 
 			mov r0, r9
 
 			mov r3, #2 @; pasem a r3 la orientació que volem que miri, oest i nord (2 i 3)
 
+
+
+			
+			
+			
+			sub r11, #1		@; gastem un intent de recombinació
+			cmp r11, #0
+			beq .Linici_recomb @; tornem a començar de nou la recombinació si es supera els intents màxims
+
 			bl cuenta_repeticiones
 			cmp r0, #3
 			bhs .Lrecombinacio	@; torna a intentar amb altre element si forma secuència
 
+			mov r0, r9
 			add r3, #1
 			bl cuenta_repeticiones
 			cmp r0, #3
@@ -234,24 +246,21 @@ recombina_elementos:
 
 
 
+			.Lseguent_iteracio_base:		
 
-			mov r5, #0
-			strb r5, [r8, r0]	@; fixem 0 a la posició visitada de mat_recomb1[][]
-
-			.seguent_iteracio_base:
-				
-				add r1, #1
-				cmp r1, #COLUMNS
-				bne .lb_seguentcolumna
+				mov r10, #COLUMNS
+				mla r10, r1, r10, r2
+				strb r4, [r9, r10]
 
 				add r2, #1
-				cmp r2, #ROWS
+				cmp r2, #COLUMNS
+
+				bne .lb_seguentcolumna
+
+				add r1, #1
+				cmp r1, #ROWS
 				bne .lb_seguentfila
 
-				
-
-			@; TODO: Falta revisar si es imposible recombinar sin generar una secuencia en las últimas casillas
-		
 		@; Còpia de mat_recomb2[][] a r0
 		mov r3, #0
 		.Lcopia_final:
@@ -261,7 +270,7 @@ recombina_elementos:
 			cmp r3, #ROWS*COLUMNS
 			blo .Lcopia_final
 
-		pop {r0-r7, pc}
+		pop {r0-r11, pc}
 
 
 
@@ -323,5 +332,4 @@ random:
 
 
 .end
-
 
